b7f32c817c77f64dee4e47da988d97d58155ff18
beni001 <88925406+beni001@users.noreply.github.com>
2024-02-02 13:18:12 +0300

    Update vite.config.js
    base for deployment
    
    Signed-off-by: beni001 <88925406+beni001@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // set base to '/' for production and '/<REPO>/' for development
+  const base = mode === 'production' ? '/' : '/<REPO>/'
+  return {
+    base,
+    plugins: [react()],
+  }
 })
+
